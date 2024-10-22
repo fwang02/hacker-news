32eb3dccc39e0c7e4251c6e03bfe7bb8aa654bd2
angellamaecs <angella.mae.santiago@estudiantat.upc.com>
2024-10-22 19:55:55 +0200

    corregido espacio entre botones

--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -8,5 +9,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('create_account/', views.create_account, name='create_account'),
     path('logout/', views.logout, name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 
 ]
