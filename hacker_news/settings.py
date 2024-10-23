751415187c41e69297fbf08591f5f6c7753f35cd
angellamaecs <angella.mae.santiago@estudiantat.upc.com>
2024-10-23 23:12:23 +0200

    cabecera añadida

--- hacker_news/settings.py
+++ hacker_news/settings.py
@@ -172,6 +172,10 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
     # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
