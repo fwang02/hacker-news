cd3087249b13ddc0d1e02fa475137fb5a159d085
angellamaecs <angella.mae.santiago@estudiantat.upc.com>
2024-10-23 12:02:38 +0200

    desde 0

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Submission(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return f"/news/{self.id}"
