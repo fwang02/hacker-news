294396cff3e83d2b9d4b840c17fa15ca087f8484
angellamaecs <angella.mae.santiago@estudiantat.upc.com>
2024-10-24 10:54:38 +0200

    url no obligatorio

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from news.models import Submission
+
 # Register your models here.
+admin.site.register(Submission)
--- news/models.py
+++ news/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 class Submission(models.Model):
     title = models.CharField(max_length=200)
-    url = models.URLField()
-    text = models.TextField()
+    url = models.URLField(blank=True, null=True)
+    text = models.TextField()  # Ensure this field is defined
     created = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.title
+
     def get_absolute_url(self):
         return f"/news/{self.id}"
