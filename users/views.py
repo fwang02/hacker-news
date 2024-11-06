8e23f641a7c30983c6335fe37a55aa0327f688b4
angellamaecs <angella.mae.santiago@estudiantat.upc.com>
2024-11-06 21:17:06 +0100

    perfil publico creado

--- users/views.py
+++ users/views.py
@@ -9,16 +9,17 @@ def profile(request):
     user_id = request.GET.get('id')
     user = get_object_or_404(User, username=user_id) if user_id else request.user
 
-    if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=user.profile)
-        if form.is_valid():
-            form.save()
-            return redirect('users:profile')
+    if request.user == user:
+        if request.method == 'POST':
+            form = ProfileForm(request.POST, instance=user.profile)
+            if form.is_valid():
+                form.save()
+                return redirect('users:profile')
+        else:
+            form = ProfileForm(instance=user.profile)
+        return render(request, 'profile.html', {'form': form, 'username': user.username})
     else:
-        form = ProfileForm(instance=user.profile)
-
-    return render(request, 'profile.html', {'form': form, 'username': user.username})
-
+        return render(request, 'profile_public.html', {'user': user})
 
 def submissions(request):
     user_id = request.GET.get('id')
