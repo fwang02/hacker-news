232db171b01ce89ac488c9c3cc59c2e2a5f770e1
angellamaecs <angella.mae.santiago@estudiantat.upc.com>
2024-11-06 03:16:28 +0100

    se guarda bien el about

--- users/views.py
+++ users/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .forms import ProfileForm
+
 
 @login_required
 def profile(request):
+    user_id = request.GET.get('id')
+    user = get_object_or_404(User, username=user_id) if user_id else request.user
+
     if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=request.user.profile)
+        form = ProfileForm(request.POST, instance=user.profile)
         if form.is_valid():
             form.save()
-            return redirect('profile')
+            return redirect('users:profile')
     else:
-        form = ProfileForm(instance=request.user.profile)
-    return render(request, 'profile.html', {'form': form, 'username': request.user.username})
+        form = ProfileForm(instance=user.profile)
+
+    return render(request, 'profile.html', {'form': form, 'username': user.username})
