30ee2b0cd7bc5f921b5bf9bbf52c8713f5d6ec49
angellamaecs <angella.mae.santiago@estudiantat.upc.com>
2024-11-06 21:05:20 +0100

    corregido submission solo title

--- news/forms.py
+++ news/forms.py
@@ -15,3 +15,12 @@ class SubmissionForm(forms.ModelForm):
         super(SubmissionForm, self).__init__(*args, **kwargs)
         for field in self.fields.values():
             field.label_suffix = ''  # Remove the colon
+
+    def clean(self):
+        cleaned_data = super().clean()
+        url = cleaned_data.get('url')
+        text = cleaned_data.get('text')
+
+        if not url and not text:
+            raise forms.ValidationError("At least one of URL or text must be provided.")
+        return cleaned_data
