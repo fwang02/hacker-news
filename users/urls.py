89679ec00a9e55000bb0925e91d40b6a05a04857
angellamaecs <angella.mae.santiago@estudiantat.upc.com>
2024-11-07 18:13:24 +0100

    vista hidden creada

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('submissions/', views.submissions, name='submissions'),
-
+    path('hidden/', views.hidden_submissions, name='hidden_submissions'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .forms import ProfileForm
-from news.models import Submission  # Assuming you have a Submission model in the news app
+from news.models import Submission, HiddenSubmission  # Assuming you have a Submission model in the news app
 
 def profile(request):
     user_id = request.GET.get('id')
@@ -38,3 +38,12 @@ def submissions(request):
     user = get_object_or_404(User, username=user_id)
     submissions = Submission.objects.filter(author=user)  # Usa el nombre correcto del campo
     return render(request, 'submissions.html', {'submissions': submissions, 'username': user.username})
+
+
+@login_required
+def hidden_submissions(request):
+    hidden_submissions_ids = HiddenSubmission.objects.filter(user=request.user).values_list('submission', flat=True)
+    print(f"Hidden Submissions IDs: {hidden_submissions_ids}")  # Debugging print statement
+    submissions = Submission.objects.filter(id__in=hidden_submissions_ids)
+    print(f"Submissions: {submissions}")  # Debugging print statement
+    return render(request, 'hidden.html', {'submissions': submissions, 'username': request.user.username})
