d244f66e91481b3073c5360567b2a691621849fe
fwang02 <fywang021205@gmail.com>
2024-11-13 15:15:35 +0100

    Añadir atributo comment_count al modelo Submission

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from news.models import Submission, Comment
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
