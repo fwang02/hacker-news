bd4abf24b5432dbbaed23f98fe34206410ccb4a6
fwang02 <fywang021205@gmail.com>
2024-11-13 13:01:38 +0100

    Fix error

--- users/urls.py
+++ users/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
     path('submissions/', views.submissions, name='submissions'),
     path('hidden/', views.hidden_submissions, name='hidden_submissions'),
     path('unhide/<int:submission_id>/', views.unhide_submission, name='unhide_submission'),
-    path('upvote/<int:submission_id>/', views.upvote, name='upvote'),
-    path('unvote/<int:submission_id>/', views.unvote, name='unvote'),
+    path('upvote/submission/<int:submission_id>/', views.upvote, name='upvote_submission'),
+    path('unvote/submission/<int:submission_id>/', views.unvote, name='unvote_submission'),
     path('upvoted/', views.upvoted_submissions, name='voted_submissions'),
     path('favorites/', views.favorites, name='favorites'),
-    path('favorite/<int:submission_id>', views.add_favorite_submission, name='add_favorite_submission'),
-    path('favorite/<int:comment_id>', views.add_favorite_comment, name='add_favorite_comment'),
-    path('unfavorite/<int:submission_id>', views.remove_favorite_submission, name='remove_favorite_submission'),
-    path('unfavorite/<int:comment_id>', views.remove_favorite_comment, name='remove_favorite_comment'),
+    path('favorite/submission/<int:submission_id>', views.add_favorite_submission, name='add_favorite_submission'),
+    path('favorite/comment/<int:comment_id>', views.add_favorite_comment, name='add_favorite_comment'),
+    path('unfavorite/submission/<int:submission_id>', views.remove_favorite_submission, name='remove_favorite_submission'),
+    path('unfavorite/comment/<int:comment_id>', views.remove_favorite_comment, name='remove_favorite_comment'),
 
 
 ]
