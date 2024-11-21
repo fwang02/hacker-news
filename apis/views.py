77e6cbee2e1ecd6d196a4602ac1d915a71e49965
fwang02 <fywang021205@gmail.com>
2024-11-21 14:50:33 +0100

    Tasks 198,199,201

--- apis/views.py
+++ apis/views.py
@@ -3,13 +3,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from news.models import Submission, Comment
+from news.utils import calculate_score
 from .serializers import SubmissionSerializer, CommentSerializer
 
-
-# Create your views here.
 class Submission_APIView(APIView):
     def get(self, request):
+        sort = request.query_params.get('sort', 'score')
         submissions = Submission.objects.all()
+
+        if sort == 'score':
+            submissions = sorted(submissions, key=lambda x: calculate_score(x), reverse=True)
+        elif sort == 'newest':
+            submissions = submissions.order_by('-created')
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'error': 'Invalid sort parameter'})
+
         serializer = SubmissionSerializer(submissions, many=True)
         return Response(serializer.data)
 
@@ -18,4 +26,5 @@ class Comment_APIView(APIView):
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
+
 
