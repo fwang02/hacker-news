00ec749155f398a835ea6d9aaf447ddf522b29bc
angellamaecs <angella.mae.santiago@estudiantat.upc.com>
2024-11-27 11:04:48 +0100

    todos los errores de post new submission añadido

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from news.models import Submission, Comment
+from news.models import Submission, Comment, Submission_ASK, Submission_URL
 
 class ReplySerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,6 +31,18 @@ class SubmissionCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = ['title', 'url', 'text']
+
+    def create(self, validated_data):
+        if 'url' in validated_data and validated_data['url']:
+            submission = Submission_URL.objects.create(**validated_data)
+        else:
+            submission = Submission_ASK.objects.create(**validated_data)
+        return submission
+
+    def validate(self, data):
+        if not data.get('url') and not data.get('text'):
+            raise serializers.ValidationError("Either 'url' or 'text' must be provided.")
+        return data
 
     def validate_title(self, value):
         #Ensure the title is unique.
